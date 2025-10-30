6c3b92f087c55fa391b9f98cc088212ef573c8f2
NoahBradberry <nbradberry425@phm.k12.in.us>
2025-10-30 09:49:52 -0400

    Update mnist_practice.py

--- mnist_practice.py
+++ mnist_practice.py
@@ -4,14 +4,15 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import random
 # Print version to verify tf is installed
 print(tf.__version__)
 
 mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 # x = images, y = numbers
-sns.countplot(x=y_train)
-plt.show()
+#sns.countplot(x=y_train)
+#plt.show()
 
 #Check to make sure there are no values that are NAN (Not a Number)
 
@@ -27,3 +28,30 @@ x_train = x_train/255.0 #Normalize the data to be between 0 and 1
 #same for testing
 x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], x_test.shape[2], 1)
 x_test = x_test/255.0 #Normalize the data to be between 0 and 1
+
+#Convert our labels to be ONE-HOT, not sparse
+y_train = tf.one_hot(y_train.astype(np.int32), depth = 10)
+y_test = tf.one_hot(y_test.astype(np.int32), depth = 10)
+
+#Show an example image from Mnist
+plt.imshow(x_train[random.randint(0, 59999)][:,:,0], cmap= "gray")
+plt.show()
+
+batch_size = 128
+num_classes = 10
+epochs = 5
+
+#Build the Model
+model = tf.keras.models.Sequential(
+    [
+        tf.keras.layers.Conv2D(32, (5,5), padding = 'same', activation = 'relu', input_shape = input_shape),
+        tf.keras.layers.Conv2D(32, (3,3), padding = 'same', activation = 'relu', input_shape = input_shape),
+        tf.keras.layers.MaxPool2D(),
+        tf.keras.layers.Dropout(0.25),
+        tf.keras.layers.Conv2D(64, (3,3), padding = 'same', activation = 'relu', input_shape = input_shape),
+        tf.keras.layers.Conv2D(64, (3,3), padding = 'same', activation = 'relu', input_shape = input_shape),
+        tf.keras.layers.Dense(num_classes, activation = 'softmax')
+    ]
+)
+
+model.compile(optimizer = tf.keras.optimizers.RMes epsilon = 1e-60  'categorical_cross_entropy', )
