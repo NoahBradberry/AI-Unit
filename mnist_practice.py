5954af5212da2616114953af8b32bfb92291db59
NoahBradberry <nbradberry425@phm.k12.in.us>
2025-11-03 09:50:11 -0500

    Update mnist_practice.py

--- mnist_practice.py
+++ mnist_practice.py
@@ -8,7 +8,7 @@ import random
 # Print version to verify tf is installed
 print(tf.__version__)
 
-mnist = tf.keras.datasets.mnist
+mnist = tf.keras.datasets.mnist 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 # x = images, y = numbers
 #sns.countplot(x=y_train)
@@ -50,8 +50,31 @@ model = tf.keras.models.Sequential(
         tf.keras.layers.Dropout(0.25),
         tf.keras.layers.Conv2D(64, (3,3), padding = 'same', activation = 'relu', input_shape = input_shape),
         tf.keras.layers.Conv2D(64, (3,3), padding = 'same', activation = 'relu', input_shape = input_shape),
+        tf.keras.layers.Flatten(),
         tf.keras.layers.Dense(num_classes, activation = 'softmax')
     ]
 )
 
-model.compile(optimizer = tf.keras.optimizers.RMes epsilon = 1e-60  'categorical_cross_entropy', )
+model.compile(optimizer = tf.keras.optimizers.RMSprop( epsilon = 1e-08),  loss ='categorical_crossentropy', metrics = ['acc'] )
+
+history = model.fit(x_train, y_train, batch_size = batch_size, epochs = epochs, validation_data = (x_test, y_test))
+
+#Plot out traing and validation accuracy and loss
+fig, ax = plt.subplot(2,1)
+
+ax[0].plot(history.history['loss'], color = 'b', label = 'Training Loss')
+ax[0].plot(history.history['val_loss'], color = 'r', label = 'Validation Loss')
+legend = ax[0].legend(loc = 'best', shadow = True)
+ax[0].set_title("Loss")
+ax[0].set_xlabel("Epochs")
+ax[0].set_ylabel("Loss")
+
+ax[1].plot(history.history['acc'], color = 'b', label = 'Training Acuracy')
+ax[1].plot(history.history['val_acc'], color = 'r', label = 'Validation Acuracy')
+legend = ax[1].legend(loc = 'best', shadow = True)
+ax[1].set_title("Accuracy")
+ax[1].set_xlabel("Epochs")
+ax[1].set_ylabel("Acuracy")
+
+plt.tight_layout()
+plt.show()
